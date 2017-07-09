2234e1a0e947d73b64c06b1d58a7cff991329f56
Raphaël Yancey <raphael@badfile.net>
2017-07-09 21:42:52 +0200

    PoC with new algorithm.

--- app.js
+++ app.js
@@ -1,108 +1,43 @@
 var app = angular.module('recordGrader', []);
 
-app.controller('MainController', ['$scope', function($scope) {
+app.controller('MainController', ['$scope', '$http', function($scope, $http) {
 
-	$scope.record = {
+	$scope.answers = {};
 
-		type: null,
-		sealed: null,
+	var orderedGrades = ['P', 'G', 'VG', 'VG+', 'NM', 'M'];
 
-		grade: null,
-
-		cover: {
-			ringWorn: null,
-			worn: null,
-			violenced: null,
-
-			grade: null
-		},
-
-		disc: {
-			played: null,
-			skipping: null,
-			warped: null,
-			noisy: null,
-			scratched: null,
-
-			grade: null
-		}
-	};
-
-	var reset = angular.copy($scope.record);
-
-	$scope.$watch('record.sealed', function(sealed) {
-		if(sealed) {
-			$scope.record.disc.played = false;
-			$scope.record.disc.skipping = false;
-			$scope.record.disc.noisy = false;
-			$scope.record.disc.warped = false;
-			$scope.record.disc.scratched = false;
-		}
+	$http.get('./grades.json').then(function(res) {
+		$scope.criterions = res.data;
+	}).catch(function() {
+		throw new Error("Couldn't get the grades.");
 	});
 
-	$scope.$watchCollection('record.cover', function(cover) {
+	function computeGrade(answers, criterions, orderedGrades, callback) {
+		
+		var gradePosition = Object.keys(answers).reduce(function(prev, key) {	
+			console.group();
+			console.log('— key', key);
+			var criterion = _.findWhere(criterions, { prop: key });
+			console.log('— criterion', criterion);
+			var gradeAnswer = criterion.rules[answers[key]];
+			console.log('— gradeAnswer', gradeAnswer);
+			var gradeAnswerPos = orderedGrades.indexOf(gradeAnswer);
+			console.log('— gradeAnswerPos', gradeAnswerPos);
+			if(gradeAnswerPos < prev) return gradeAnswerPos;
+			else return prev;
+			console.groupEnd();
+		}, (orderedGrades.length - 1));
 
-		var grades = [
-			{ code: 'M', total: 0 },
-			{ code: 'NM', total: 0 },
-			{ code: 'VG+', total: 1 },
-			{ code: 'VG', total: 2 },
-			{ code: 'G+', total: 3 },
-			{ code: 'G', total: 4 },
-			{ code: 'F', total: 5 },
-			{ code: 'P', total: 6 }
-		];
+		console.log('— gradePosition', gradePosition);
 
-		if(!_.isNull(cover.ringWorn) && !_.isNull(cover.worn) && !_.isNull(cover.violenced)) {
-			
-			var total = cover.ringWorn + cover.worn + cover.violenced;
-			var grade;
+		callback(orderedGrades[gradePosition]);
+	}
 
-			// Record is `Mint` only if the total is 0 and the record is sealed
-			if(total == 0)
-				grade = $scope.record.sealed ? _.findWhere(grades, { code: 'M' }) : _.findWhere(grades, { code: 'NM' });
-			else				
-				grade = _.findWhere(grades, { total: total });
-			
-			cover.grade = grade.code;
-		}
+	$scope.$watchCollection('answers', function(n) {
+		console.log('— n', n);
+		computeGrade(n, $scope.criterions, orderedGrades, function(res) {
+			$scope.result = res;
+		});
 	});
 
-	$scope.$watchCollection('record.disc', function(disc) {
-
-		var grades = [
-			{ code: 'M', total: 0 },
-			{ code: 'NM', total: 0 },
-			{ code: 'VG+', total: 1 },
-			{ code: 'VG', total: 2 },
-			{ code: 'VG', total: 3 },
-			{ code: 'G+', total: 4 },
-			{ code: 'G+', total: 5 },
-			{ code: 'G', total: 6 },
-			{ code: 'G', total: 7 },
-			{ code: 'F', total: 8 },
-			{ code: 'F', total: 9 },
-			{ code: 'P', total: 10 }
-		];
-
-		// If the disc hasn't been played, it can't skip or be noisy (can it?)
-		if(disc.played == 0) {
-			disc.noisy = 0;
-			disc.skipping = 0;
-		}
-
-		if(!_.isNull(disc.played) && !_.isNull(disc.warped) && !_.isNull(disc.noisy) && !_.isNull(disc.scratched) && !_.isNull(disc.skipping)) {
-			
-			var total = disc.played + disc.warped + disc.noisy + disc.scratched + disc.skipping;
-			var grade;
-
-			// Record is `Mint` only if the total is 0 and the record is sealed
-			if(total == 0)
-				grade = $scope.record.sealed ? _.findWhere(grades, { code: 'M' }) : _.findWhere(grades, { code: 'NM' });
-			else				
-				grade = _.findWhere(grades, { total: total });
-			
-			disc.grade = grade.code;
-		}
-	});
 }]);
