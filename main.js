2b7692f1bdf30b30fb14bcc7bf875ef065cd15c6
Raphaël Yancey <raphael@badfile.net>
2020-10-14 12:04:50 +0200

    Refactoring with React

--- main.js
+++
@@ -1,9 +0,0 @@
-require('./app.js');
-require('./dir_rg-questions.js');
-require('./dir_rg-random-cover.js');
-
-$(function () {
-  $('[data-toggle="tooltip"]').tooltip({
-    container: 'body'
-  })
-});
